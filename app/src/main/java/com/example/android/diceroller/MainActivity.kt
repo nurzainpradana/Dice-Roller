cae4d4c118e9c2b73382e96c5a3403d7c5910ba3
nurzainpradana <pradananurzain@gmail.com>
2020-10-26 10:50:57 +0700

    update text view menjadi image view, menggunakan library AndroidX dengan srcCompat

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.android.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,19 +15,24 @@ aplikasi kita available untuk perangkat Android yang dengan jumlah terbanyak dan
 pengguna, selalu gunakan AppCompatActivity
  */
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage : ImageView
+    lateinit var rollButton: Button
+    //lateinit var countUpButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //Inisialisasi View
-        val rollButton: Button = findViewById(R.id.roll_button)
-        val countUpButton: Button = findViewById(R.id.count_up_button)
+        //val countUpButton: Button = findViewById(R.id.count_up_button)
+        rollButton = findViewById(R.id.roll_button)
+        diceImage = findViewById(R.id.dice_image)
 
         rollButton.setOnClickListener { rollDice() }
-        countUpButton.setOnClickListener { countUp() }
+        // countUpButton.setOnClickListener { countUp() }
     }
 
+    /*
     private fun countUp() {
            var text = result_text.text.toString()
         if (text == getString(R.string.hello_world_text)){
@@ -40,11 +46,25 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+     */
+
     private fun rollDice(){
         //Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
 
         //Variabel untuk mendapatkan angka random dari 1 sampai 6 secara otomatis
         val randomInt = (1..6).random()
-        result_text.text = randomInt.toString()
+        //result_text.text = randomInt.toString()
+        Toast.makeText(this, "Dadu $randomInt", Toast.LENGTH_SHORT).show()
+
+        val drawableResource = when (randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
     }
 }
